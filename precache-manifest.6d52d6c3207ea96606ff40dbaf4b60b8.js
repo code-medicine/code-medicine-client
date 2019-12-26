13b5e4cddab66f2353c0ac210f954cb1b0a81397
FarrukhShahid <farrukh.shahid0@gmai.com>
2019-12-26 17:08:33 +0500

    Updates

---
+++ precache-manifest.6d52d6c3207ea96606ff40dbaf4b60b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a98c9ef54a19817eabbc116906878cb3",
+    "url": "/code-medicine-client/index.html"
+  },
+  {
+    "revision": "f64dd7256f85db8c48d4",
+    "url": "/code-medicine-client/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "df2a617ebf67f3b05e47",
+    "url": "/code-medicine-client/static/js/2.ce3de43d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/code-medicine-client/static/js/2.ce3de43d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f64dd7256f85db8c48d4",
+    "url": "/code-medicine-client/static/js/main.7077e7bd.chunk.js"
+  },
+  {
+    "revision": "072a5294b29d1f5aef41",
+    "url": "/code-medicine-client/static/js/runtime-main.4c679aa5.js"
+  }
+]);
