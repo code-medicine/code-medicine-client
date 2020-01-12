9d4805540eae6db67e15c98c82f580cee59c0528
FarrukhShahid <farrukh.shahid0@gmail.com>
2020-01-13 00:44:18 +0500

    middle ware implimented for authentication

--- project/src/components/home/home.js
+++ project/src/components/home/home.js
@@ -2,6 +2,9 @@ import React, {Component} from 'react';
 import Container from '../../shared/container/container';
 import { LOGIN_URL } from '../../shared/router_constants';
 import Axios from 'axios';
+import { PROFILE_USER_REQUEST } from '../../shared/rest_end_points';
+import { connect } from "react-redux";
+import { notify } from '../../actions';
 
 class Home extends Component {
     constructor(props){
@@ -16,7 +19,17 @@ class Home extends Component {
             this.props.history.push(LOGIN_URL)
         }
         else{
-            // Axios.get()
+            Axios.get(`${PROFILE_USER_REQUEST}?tag=${localStorage.user_token}`).then(res => {
+                if (!res.data['status']){
+                    this.props.notify('default','',res.data['message'])
+                    this.props.history.push(LOGIN_URL)
+                }
+                else{
+                    this.props.notify('success','',res.data['message'])
+                    console.log(res.data['payload']);
+                    // -----------------todo: data received! do what ever with this data now!
+                }
+            })
         }
     }
 
@@ -28,4 +41,7 @@ class Home extends Component {
         );
     }
 }
-export default Home;
+function map_state_to_props(notify) {
+    return { notify }
+}
+export default connect(map_state_to_props, { notify })(Home);
--- project/src/shared/rest_end_points.js
+++ project/src/shared/rest_end_points.js
@@ -2,7 +2,8 @@ const PROTOCOL = 'http';
 const ADDRESS = 'localhost';
 const PORT = '5000';
 
-export const BASE_URL = `${PROTOCOL}://${ADDRESS}:${PORT}`
+export const BASE_URL = `${PROTOCOL}://${ADDRESS}:${PORT}`;
 export const BASE_USERS_URL = `${BASE_URL}/users`;
-export const REGISTER_USER_REQUEST = `${BASE_USERS_URL}/register`
-export const LOGIN_USER_REQUEST = `${BASE_USERS_URL}/login`
+export const REGISTER_USER_REQUEST = `${BASE_USERS_URL}/register`;
+export const LOGIN_USER_REQUEST = `${BASE_USERS_URL}/login`;
+export const PROFILE_USER_REQUEST = `${BASE_USERS_URL}/profile`;
