bc7e18b64884b5d9b763acc30a3b2c654a75611e
FarrukhShahid <farrukh.shahid0@gmail.com>
2020-01-12 17:45:39 +0500

    loading in login

--- project/src/components/login/login.js
+++ project/src/components/login/login.js
@@ -14,6 +14,7 @@ class Login extends Component {
             email: '',
             password: '',
             remember_me_option: true,
+            loading_status: false,
         }
     }
 
@@ -34,12 +35,17 @@ class Login extends Component {
     }
 
     on_submit = (e) => {
+        this.setState({loading_status: true})
         const data = {
             email: this.state.email.trim(),
             password: this.state.password.trim(),
             remember_me: this.state.remember_me_option
         }
         Axios.post(LOGIN_USER_REQUEST,data).then(res => {
+            setTimeout(() => {
+                this.setState({ loading_status: false })
+                this.props.history.push(LOGIN_URL)
+            }, 5000)
             if (res.data['status']){
                 localStorage.setItem("user_token",res.data['token'])
                 this.props.notify('success','',"successfully logged in")
@@ -55,81 +61,98 @@ class Login extends Component {
     render() {
 
         var view = ''
-
-        view = <div className="container-fluid">
-        <div className="row">
-            <div className="col-md-2 col-lg-4 col-sm-0">
-
+        if (this.state.loading_status){
+        view = <div className="mt-3">
+                <div className="d-flex justify-content-center ">
+                    <Loader
+                        type="Rings"
+                        color="#00BFFF"
+                        height={150}
+                        width={150}
+                        timeout={60000} //60 secs
+                    />
+                </div>
+                <div className="d-flex justify-content-center">
+                    <p>Loading...</p>
+                </div>
             </div>
-            <div className="col-md-8 col-lg-4 col-sm-12">
-                <div className="card mt-3">
-                    <h5 className="card-header info-color white-text text-center py-4">
-                        <strong>Sign in</strong>
-                    </h5>
-                    <div className="card-body px-lg-5 pt-0">
-                        <div
-                            className="text-center" 
-                            autoComplete="nope" 
-                            style={{ color: "#757575" }}>
-                            <div className="md-form">
-                                <input 
-                                    type="email" 
-                                    id="materialLoginFormEmail" 
-                                    className="form-control"
-                                    value={this.state.email}
-                                    onChange={this.on_text_change} />
-                                <label for="materialLoginFormEmail">E-mail</label>
-                            </div>
-                            <div className="md-form">
-                                <input 
-                                    type="password" 
-                                    id="materialLoginFormPassword" 
-                                    className="form-control"
-                                    value={this.state.password}
-                                    onChange={this.on_text_change} />
-                                <label for="materialLoginFormPassword">Password</label>
-                            </div>
-                            <div className="d-flex justify-content-around">
-                                <div>
-                                    <div className="form-check">
+        }
+        else{
+            view = <div className="container-fluid">
+                <div className="row">
+                    <div className="col-md-2 col-lg-4 col-sm-0">
+
+                    </div>
+                    <div className="col-md-8 col-lg-4 col-sm-12">
+                        <div className="card mt-3">
+                            <h5 className="card-header info-color white-text text-center py-4">
+                                <strong>Sign in</strong>
+                            </h5>
+                            <div className="card-body px-lg-5 pt-0">
+                                <div
+                                    className="text-center" 
+                                    autoComplete="nope" 
+                                    style={{ color: "#757575" }}>
+                                    <div className="md-form">
                                         <input 
-                                            type="checkbox" 
-                                            className="form-check-input" 
-                                            id="materialLoginFormRemember"
-                                            defaultChecked={this.state.remember_me_option}
+                                            type="email" 
+                                            id="materialLoginFormEmail" 
+                                            className="form-control"
+                                            value={this.state.email}
                                             onChange={this.on_text_change} />
-                                        <label 
-                                            className="form-check-label" 
-                                            for="materialLoginFormRemember">
-                                            Remember me
-                                        </label>
+                                        <label for="materialLoginFormEmail">E-mail</label>
                                     </div>
-                                </div>
-                                <div>
-                                    <a href={LOGIN_URL}>Forgot password?</a>
+                                    <div className="md-form">
+                                        <input 
+                                            type="password" 
+                                            id="materialLoginFormPassword" 
+                                            className="form-control"
+                                            value={this.state.password}
+                                            onChange={this.on_text_change} />
+                                        <label for="materialLoginFormPassword">Password</label>
+                                    </div>
+                                    <div className="d-flex justify-content-around">
+                                        <div>
+                                            <div className="form-check">
+                                                <input 
+                                                    type="checkbox" 
+                                                    className="form-check-input" 
+                                                    id="materialLoginFormRemember"
+                                                    defaultChecked={this.state.remember_me_option}
+                                                    onChange={this.on_text_change} />
+                                                <label 
+                                                    className="form-check-label" 
+                                                    for="materialLoginFormRemember">
+                                                    Remember me
+                                                </label>
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <a href={LOGIN_URL}>Forgot password?</a>
+                                        </div>
+                                    </div>
+                                    <button 
+                                        className="btn btn-outline-info btn-rounded btn-block my-4 waves-effect z-depth-0" 
+                                        type="submit"
+                                        onClick={this.on_submit}>
+                                        Sign in
+                                    </button>
+                                    <p style={{ fontFamily: 'Roboto' }}>
+                                        Not a member?
+                                        <a href="/register" > 
+                                            Register
+                                        </a>
+                                    </p>
                                 </div>
                             </div>
-                            <button 
-                                className="btn btn-outline-info btn-rounded btn-block my-4 waves-effect z-depth-0" 
-                                type="submit"
-                                onClick={this.on_submit}>
-                                Sign in
-                            </button>
-                            <p style={{ fontFamily: 'Roboto' }}>
-                                Not a member?
-                                <a href="/register" > 
-                                    Register
-                                </a>
-                            </p>
                         </div>
+                    </div>
+                    <div className="col-md-2 col-lg-4 col-sm-0">
+
                     </div>
                 </div>
             </div>
-            <div className="col-md-2 col-lg-4 col-sm-0">
-
-            </div>
-        </div>
-    </div>
+        }
 
         return (
             <Container header={false} footer={true}>
