a45b05bd594ddbf39c153d9b101eeca3c5404722
FarrukhShahid <farrukh.shahid0@gmail.com>
2020-08-26 10:22:35 +0500

    COD-71 v2.0.1 build folder was not commited

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b53fc48892cac59de549e7e7d30e0016.js"
+  "/precache-manifest.9278f807ee66c9671b39e4c98d09e154.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
